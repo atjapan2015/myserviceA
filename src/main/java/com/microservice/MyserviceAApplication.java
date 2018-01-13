d27d58ec9a28453cbd175a5ff61328734b1d31c5
oracle <oracle@localhost.localdomain>
2018-01-13 08:20:52 -0500

    update

--- src/main/java/com/microservice/MyserviceAApplication.java
+++ src/main/java/com/microservice/MyserviceAApplication.java
@@ -3,6 +3,8 @@ package com.microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.microservice.myserviceA.register.ConsulRegisterListener;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -11,6 +13,7 @@ public class MyserviceAApplication {
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(MyserviceAApplication.class);
+        application.addListeners(new ConsulRegisterListener());
         application.run(args);
     }
 }
